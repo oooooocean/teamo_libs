716df4f963890631d4f5125f73ac5a81ec966b1a
zzz <1038883524@qq.com>
2024-06-10 09:04:48 +0800

    patch

--- net/lib/request_mixin.dart
+++ net/lib/request_mixin.dart
@@ -25,6 +25,15 @@ mixin RequestMixin {
             test: (error) => error is DioException);
   }
 
+  Future<T> patch<T>(String uri, dynamic body, Decoder<T> decoder, {Map<String, dynamic>? query}) async {
+    return await _net
+        .patch(uri,
+            data: body, options: Options(contentType: 'application/json'), queryParameters: _correctParameters(query))
+        .then((res) => _parse(res.data, decoder))
+        .catchError((error) => _parse((error as DioException).response?.data, decoder),
+            test: (error) => error is DioException);
+  }
+
   Future<T> put<T>(String uri, dynamic body, Decoder<T> decoder, {Map<String, dynamic>? query}) async {
     return await _net
         .put(uri,
@@ -42,7 +51,8 @@ mixin RequestMixin {
 
   Future<T> uploadFiles<T>(List<Uint8List> files, String path, Decoder<T> decoder,
       {Map<String, dynamic>? query}) async {
-    final formData = FormData.fromMap({'files': files.map((e) => MultipartFile.fromBytes(e, filename: 'file.jpeg')).toList()});
+    final formData =
+        FormData.fromMap({'files': files.map((e) => MultipartFile.fromBytes(e, filename: 'file.jpeg')).toList()});
     return await _net
         .post(path, data: formData, queryParameters: query)
         .then((res) => _parse(res.data, decoder))
