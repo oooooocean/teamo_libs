170dd2c8b834044e0db1b9bf944342eddd320b20
陈强 <chenqiang19901202@163.com>
2025-06-24 17:21:13 +0800

    feat | s3

--- net/lib/s3_mixin.dart
+++ net/lib/s3_mixin.dart
@@ -1,15 +1,7 @@
 import 'dart:math';
 import 'dart:typed_data';
-import 'package:flutter/material.dart';
-import 'package:s3_storage/s3_storage.dart';
-
-final s3 = S3Storage(
-  endPoint: 's3.amazonaws.com',
-  accessKey: 'AKIA5FTZDOAFJPRTKUBP',
-  secretKey: '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',
-  region: 'us-east-1',
-  signingType: SigningType.V4,
-);
+import 'package:dio/dio.dart';
+import 'package:net/net_dio.dart';
 
 const bucket = 'public-teamo-com';
 
@@ -19,7 +11,8 @@ mixin S3Mixin {
   Future<String?> uploadFileToS3({required Uint8List bytes, required String fileName}) async {
     final randomName = '${DateTime.now().millisecondsSinceEpoch}_${Random.secure().nextInt(1024)}_${fileName}';
     try {
-      await s3.putObject(bucket, randomName, Stream<Uint8List>.value(bytes));
+      final url = (await Net2().dio.post('file/url/', data: [randomName], options: Options(contentType: 'application/json'))).data['data'][0];
+      await Dio().putUri(Uri.parse(url), data: bytes, options: Options(contentType: 'application/octet-stream'));
       return randomName;
     } catch (error) {
       print('S3文件上传失败: $error');
@@ -27,10 +20,12 @@ mixin S3Mixin {
     }
   }
 
-  Future<String?> uploadFileStreamToS3({required Stream<Uint8List> data, required String fileName}) async {
+  Future<String?> uploadFileStreamToS3({required Stream<Uint8List> data, required String fileName, required int fileLength}) async {
     final randomName = '${DateTime.now().millisecondsSinceEpoch}_${Random.secure().nextInt(1024)}_${fileName}';
     try {
-      await s3.putObject(bucket, randomName, data);
+      final url = (await Net2().dio.post('file/url/', data: [randomName], options: Options(contentType: 'application/json'))).data['data'][0];
+      await Dio().putUri(Uri.parse(url),
+          data: data, options: Options(contentType: 'application/octet-stream', headers: {Headers.contentLengthHeader: fileLength.toString()}));
       return randomName;
     } catch (error) {
       print('S3文件上传失败: $error');
@@ -47,23 +42,6 @@ mixin S3Mixin {
       return null;
     }
     return results;
-  }
-
-  Future<List<int>?> getFileFromS3({required String fileName}) async {
-    try {
-      final stream = await s3.getObject(bucket, fileName);
-      final result = (await stream.toList()).reduce((f, s) => f + s);
-      return result;
-    } catch (error) {
-      print('S3文件下载失败失败: $error');
-      return null;
-    }
-  }
-
-  Future<ImageProvider?> getImageFromS3({required String fileName}) async {
-    final bytes = await getFileFromS3(fileName: fileName);
-    if (bytes == null) return null;
-    return MemoryImage(Uint8List.fromList(bytes));
   }
 
   String getFileUrlFromS3({required String fileName}) => 'https://public-teamo-com.s3.amazonaws.com/$fileName';
